4755e8a55cb99bc5cbfca61e14316c65d6f8c8eb
alexaNovikova <alexadubinina@mail.ru>
2025-02-11 13:30:09 +0700

    TODO удален

--- hw01/src/main/java/ru/otus/hw/Application.java
+++ hw01/src/main/java/ru/otus/hw/Application.java
@@ -7,10 +7,6 @@ import ru.otus.hw.service.TestRunnerService;
 public class Application {
     public static void main(String[] args) {
 
-        //TODO сделать unit тесты
-        //TODO добавить вопросы к списку
-        //TODO собрать в jar
-
         ApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
         var testRunnerService = context.getBean(TestRunnerService.class);
         testRunnerService.run();
