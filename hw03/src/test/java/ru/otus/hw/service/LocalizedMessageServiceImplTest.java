9b59a27cb5996921d6dfd03dbd1c720b445e09b1
alexaNovikova <alexadubinina@mail.ru>
2025-03-05 14:34:49 +0700

    Убрала лишние комментарии, добавлено тестирование локализации (класс LocalizedMessageServiceImplTest.java)

--- hw03/src/test/java/ru/otus/hw/service/LocalizedMessageServiceImplTest.java
+++ hw03/src/test/java/ru/otus/hw/service/LocalizedMessageServiceImplTest.java
@@ -37,7 +37,7 @@ public class LocalizedMessageServiceImplTest {
     }
 
     @Test
-    @DisplayName("Проверка, что сообщение выводится на английском при установленной английской локали")
+    @DisplayName("Проверка, что метод вернет сообщение на английском при установленной английской локали")
     void returnEnglishStringFoEnLocale() {
         when(localeConfig.getLocale()).thenReturn(Locale.forLanguageTag("en-US"));
         String result = localizedMessagesService.getMessage("ResultService.passed.test");
@@ -45,7 +45,7 @@ public class LocalizedMessageServiceImplTest {
     }
 
     @Test
-    @DisplayName("Проверка, что сообщение выводится на русском языке при установленной русской локали")
+    @DisplayName("Проверка, что метод вернет сообщение на русском языке при установленной русской локали")
     void returnRussianStringFoRuLocale() {
         when(localeConfig.getLocale()).thenReturn(Locale.forLanguageTag("ru-RU"));
         String result = localizedMessagesService.getMessage("ResultService.passed.test");
