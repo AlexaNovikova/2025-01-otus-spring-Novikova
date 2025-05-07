89cbe70207cb8cc34b18df22c02cbb5e79d02fbe
alexaNovikova <alexadubinina@mail.ru>
2025-05-07 16:22:28 +0700

    ДЗ 10

---
+++ hw10/src/main/java/ru/otus/hw/models/Comment.java
@@ -0,0 +1,61 @@
+package ru.otus.hw.models;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.NoArgsConstructor;
+import lombok.Builder;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "comments")
+public class Comment {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "text")
+    private String text;
+
+    @ToString.Exclude
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Comment comment)) {
+            return false;
+        }
+        return getId() == comment.getId() && Objects.equals(getText(), comment.getText());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getText());
+    }
+}
---
+++ hw10/src/main/java/ru/otus/hw/services/BookService.java
@@ -0,0 +1,20 @@
+package ru.otus.hw.services;
+
+import ru.otus.hw.dto.BookDto;
+import ru.otus.hw.dto.BookDtoWithComments;
+import ru.otus.hw.dto.BookToSaveDto;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookService {
+    Optional<BookDtoWithComments> findById(long id);
+
+    Optional<BookDto> findBookDtoById(long id);
+
+    List<BookDto> findAll();
+
+    BookDto save(BookToSaveDto bookToSaveDto);
+
+    void deleteById(long id);
+}
