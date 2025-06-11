cb4d3ae16f1943895310eaed5ecdb7138c426681
alexaNovikova <alexadubinina@mail.ru>
2025-06-11 11:29:15 +0700

    Spring Batch

--- hw13/src/test/java/ru/otus/hw/repositories/BookRepositoryTest.java
+++ hw13/src/test/java/ru/otus/hw/repositories/BookRepositoryTest.java
@@ -5,8 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import ru.otus.hw.models.Book;
 import ru.otus.hw.models.Author;
+import ru.otus.hw.models.Book;
 import ru.otus.hw.models.Genre;
 
 import java.util.Collections;
